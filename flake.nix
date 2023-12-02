2ba94183f23be7b1d924fd64155eeeca8c84cb14
Jacob Swanson <me@jacob-swanson.com>
2023-12-02 18:18:32 +0000

    Support Nix Flakes (#6)
    - Upgrade to NixOS 23.11
    - Add support for Flakes
    
    Reviewed-on: https://git.jacob-swanson.com/jacob/swandns/pulls/6
    Co-authored-by: Jacob Swanson <me@jacob-swanson.com>
    Co-committed-by: Jacob Swanson <me@jacob-swanson.com>

--- flake.nix
+++ flake.nix
@@ -1,36 +1,20 @@
 {
   description = "swandns";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nix, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        swandns = pkgs.callPackage ./. { inherit pkgs; };
-        podmanSetupScript = let
-          registriesConf = pkgs.writeText "registries.conf" ''
-            [registries.search]
-            registries = ['docker.io']
-            [registries.block]
-            registries = []
-          '';
-        in pkgs.writeScript "podman-setup" ''
-          #!${pkgs.runtimeShell}
-          # Dont overwrite customised configuration
-          if ! test -f ~/.config/containers/policy.json; then
-            install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-          fi
-          if ! test -f ~/.config/containers/registries.conf; then
-            install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-          fi
-        '';
-      in with pkgs; rec {
-        # Development environment
-        devShell = mkShell {
+  outputs = { self, nix, nixpkgs, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: function nixpkgs.legacyPackages.${system});
+    in {
+      nixosModules = { default = ./nix/swandns.nix; };
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
           name = "swandns";
-          nativeBuildInputs = [
+          nativeBuildInputs = with pkgs; [
             just
             nixfmt
             statix
@@ -42,41 +26,62 @@
             cargo-outdated
           ];
           shellHook = ''
-            ${podmanSetupScript}
+            ${let
+              registriesConf = pkgs.writeText "registries.conf" ''
+                [registries.search]
+                registries = ['docker.io']
+                [registries.block]
+                registries = []
+              '';
+            in pkgs.writeScript "podman-setup" ''
+              #!${pkgs.runtimeShell}
+              # Dont overwrite customised configuration
+              if ! test -f ~/.config/containers/policy.json; then
+                install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
+              fi
+              if ! test -f ~/.config/containers/registries.conf; then
+                install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
+              fi
+            ''}
           '';
         };
-
-        packages = {
-          default = swandns;
-          server-image = pkgs.dockerTools.buildLayeredImage {
-            name = "swandns";
-            config = {
-              Env =
-                [ "SWANDNS_CONFIG=/data/server.yaml" "SWANDNS_DATA_DIR=/data" ];
-              Cmd = [
-                "${pkgs.bash}/bin/bash"
-                "-c"
-                ''${swandns}/bin/swandns --config "''${SWANDNS_CONFIG}"''
-              ];
-              Volumes = { "/data" = { }; };
-            };
+      });
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./. { inherit pkgs; };
+        server-image = pkgs.dockerTools.buildLayeredImage {
+          name = "swandns";
+          config = {
+            Env =
+              [ "SWANDNS_CONFIG=/data/server.yaml" "SWANDNS_DATA_DIR=/data" ];
+            Cmd = [
+              "${pkgs.bash}/bin/bash"
+              "-c"
+              ''
+                ${
+                  pkgs.callPackage ./. { inherit pkgs; }
+                }/bin/swandns --config "''${SWANDNS_CONFIG}"''
+            ];
+            Volumes = { "/data" = { }; };
           };
-          client-image = pkgs.dockerTools.buildLayeredImage {
-            name = "swandns-update";
-            config = {
-              Env = [
-                "SWANDNS_CONFIG=/data/client.yaml"
-                "SWANDNS_SCHEDULE=*/5 * * * *"
-              ];
-              Cmd = [
-                "${pkgs.bash}/bin/bash"
-                "-c"
-                ''
-                  ${swandns}/bin/swandns-update --config "''${SWANDNS_CONFIG}" --schedule "''${SWANDNS_SCHEDULE}"''
-              ];
-              Volumes = { "/data" = { }; };
-            };
+        };
+        client-image = pkgs.dockerTools.buildLayeredImage {
+          name = "swandns-update";
+          config = {
+            Env = [
+              "SWANDNS_CONFIG=/data/client.yaml"
+              "SWANDNS_SCHEDULE=*/5 * * * *"
+            ];
+            Cmd = [
+              "${pkgs.bash}/bin/bash"
+              "-c"
+              ''
+                ${
+                  pkgs.callPackage ./. { inherit pkgs; }
+                }/bin/swandns-update --config "''${SWANDNS_CONFIG}" --schedule "''${SWANDNS_SCHEDULE}"''
+            ];
+            Volumes = { "/data" = { }; };
           };
         };
       });
+    };
 }
